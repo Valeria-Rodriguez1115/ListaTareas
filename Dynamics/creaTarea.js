fd9fa272728a6c85e368bcbd2aad0dbc43b91941
Valeria-Rodriguez1115 <324184767@alumno.enp.unam.mx>
2025-06-17 02:19:56 -0600

    Agrego lista de tareas completa

---
+++ Dynamics/creaTarea.js
@@ -0,0 +1,19 @@
+const botonAñade = document.getElementById("añadir");
+const botonElimina = document.getElementById("eliminar")
+const contenedor = document.getElementById("contenedor");
+botonAñade.addEventListener("click", () =>
+{
+    const nuevaTarea = document.createElement("div");
+    nuevaTarea.classList.add("tarea");
+    nuevaTarea.textContent = "Nueva tarea";
+    contenedor.appendChild(nuevaTarea);
+})
+
+botonElimina.addEventListener("click", () =>
+{
+    const tareas = document.getElementsByClassName("tarea");
+    if (tareas.length > 0)
+    {
+        contenedor.removeChild(tareas[tareas.length - 1]);
+    }
+})
